ae82b42adfdbd500958fdb008954da27e84cd136
Kwindl <115383698+Kwindl@users.noreply.github.com>
2023-03-26 20:23:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ const holes = document.querySelectorAll('.hole');
   }
 
   function startGame() {
+     alert("Look for Nacls molar mass!");
     const randemt = randomcompound(Math.floor(Math.random()*arr.length));
     alert("Look for arr[randnumb]'s molar mass!");
     scoreBoard.textContent = 0;
